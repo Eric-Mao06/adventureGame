3097dac820b99fe58f24ffc905f004ba61fce677
Eric Mao <ericmao0606@gmail.com>
2024-09-14 22:54:03 -0400

    Initial Commit

---
+++ Adventure Game/Adventure_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Adventure_GameApp.swift
+//  Adventure Game
+//
+//  Created by Eric Mao on 9/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Adventure_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Adventure GameTests/Adventure_GameTests.swift
@@ -0,0 +1,17 @@
+//
+//  Adventure_GameTests.swift
+//  Adventure GameTests
+//
+//  Created by Eric Mao on 9/14/24.
+//
+
+import Testing
+@testable import Adventure_Game
+
+struct Adventure_GameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
